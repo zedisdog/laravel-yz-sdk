84df714e3ae0a1c5b4ae166c2cc6239e71da58a3
zed <zedsmail@163.com>
2018-05-23 13:57:46 +0800

    继承

--- src/Events/redis/ReceivedYzMessage.php
+++ src/Events/redis/ReceivedYzMessage.php
@@ -9,39 +9,8 @@
 namespace Dezsidog\YzSdk\Events\redis;
 
 
-use Dezsidog\YzSdk\Message\BaseMessage;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 
-class ReceivedYzMessage implements ShouldQueue
+class ReceivedYzMessage extends \Dezsidog\YzSdk\Events\ReceivedYzMessage implements ShouldQueue
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    /**
-     * @var BaseMessage
-     */
-    public $message;
-
-    /**
-     * Create a new event instance.
-     *
-     * @param BaseMessage $message
-     */
-    public function __construct(BaseMessage $message)
-    {
-        $this->message = $message;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return PrivateChannel
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
 }
